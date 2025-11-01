9f608cd63d17c65549a9c1764587d760ed91059c
Arshdeep Singh <91834317+Arshdeep00001@users.noreply.github.com>
2025-11-01 17:30:14 +0530

    Document common curl commands in readme02.sh
    Added examples of common curl commands for data transfer.

--- 12-Shell Scripting 02/readme02.sh
+++ 12-Shell Scripting 02/readme02.sh
@@ -38,3 +38,44 @@ Eg:- echo "First line\nSecond line"
 
 -----
 
+The curl command in Linux is a versatile tool for transferring data with URLs. Here are some common sample curl commands:
+1. Basic GET Request:
+To retrieve the content of a webpage and display it in the terminal:
+Code:-
+curl https://www.example.com
+
+2. Saving Output to a File:
+To download a file and save it with a specified name:
+Code:-
+curl -o local_filename.html https://www.example.com/page.html
+
+To download a file and save it with the same name as on the remote server:
+Code
+curl -O https://www.example.com/image.jpg
+
+3. Following Redirects:
+To automatically follow HTTP redirects (e.g., from http to https or non-www to www):
+Code
+curl -L http://example.com
+
+4. Fetching HTTP Headers Only:
+To retrieve only the HTTP headers of a response without the body content:
+Code
+curl -I https://www.example.com
+
+5. Sending a POST Request with Data:
+To send data to a server using the POST method, often used for submitting forms or API requests:
+Code
+curl -X POST -H "Content-Type: application/json" -d '{"key": "value", "another_key": "another_value"}' https://api.example.com/data
+
+6. Authenticated Requests:
+To access resources requiring username and password authentication:
+Code
+curl -u username:password https://secure.example.com/protected_resource
+
+7. Uploading a File via FTP:
+To upload a file to an FTP server:
+Code
+curl -T local_file.txt -u ftp_user:ftp_pass ftp://ftp.example.com/remote_directory/
+
+------
